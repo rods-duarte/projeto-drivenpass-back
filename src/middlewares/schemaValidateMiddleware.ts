fa6695a0905225215fe53a7d00fa011e9cb28e1b
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2022-07-13 23:38:50 -0300

    Feat: add validate schema middleware

---
+++ src/middlewares/schemaValidateMiddleware.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express';
+import { Schema } from 'joi';
+import { unprocessableEntityError } from './errorHandlerMiddleware';
+
+export default function validateSchema(schema: Schema) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const body = req;
+    const { error } = schema.validate(body);
+    const valid = error == null;
+
+    if (!valid) {
+      const { details } = error;
+      const message = details.map((i) => i.message).join(',');
+
+      throw unprocessableEntityError(message);
+    }
+
+    next();
+  };
+}
