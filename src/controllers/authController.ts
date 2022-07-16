19c10f959c0d5755aa4666031c0f7ef632da8881
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2022-07-16 04:42:52 -0300

    Fix: remove sessions

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,19 +1,13 @@
 import { User } from '@prisma/client';
 import { Request, Response } from 'express';
 import authService from '../services/authService.js';
-import sessionService from '../services/sessionService.js';
 import userService, { CreateUserData } from '../services/userService.js';
 
 export async function login(req: Request, res: Response) {
   const user: User = res.locals.user;
-
-  const oldSession = await sessionService.getByUserId(user.id);
-  await sessionService.close(oldSession);
-
   const token = authService.createToken(user.id);
-  await sessionService.create({ userId: user.id, valid: true, token });
 
-  res.status(200).send(token);
+  res.status(200).send({ token });
 } //eslint-disable-line
 
 export async function register(req: Request, res: Response) {
--- src/middlewares/tokenValidateMiddleware.ts
+++ src/middlewares/tokenValidateMiddleware.ts
@@ -1,8 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
-import sessionRepository from '../repositories/sessionRepository.js';
-import authService from '../services/authService.js';
-import sessionService from '../services/sessionService.js';
 import { unauthorizedError } from './errorHandlerMiddleware.js';
 
 interface JwtPayload {
@@ -17,25 +14,14 @@ export default async function validateToken(
   next: NextFunction
 ) {
   const token = req.headers?.authorization.replace('Bearer ', '').trim();
-  const session = await sessionRepository.findByToken(token);
-
-  if (!session) {
-    const message = 'Invalid token !';
-    throw unauthorizedError(message);
-  }
 
   try {
     const data = jwt.verify(token, process.env.SECRET_KEY) as JwtPayload;
-
-    const newToken = authService.createToken(data.userId);
-    await sessionRepository.update(session.id, { token: newToken });
     res.locals.userId = data.userId;
-    res.locals.token = newToken;
-
-    next();
   } catch (err) {
-    await sessionService.close(session);
-    const message = 'Session expired !';
+    const message = 'Invalid Token !';
     throw unauthorizedError(message);
   }
+
+  next();
 }
